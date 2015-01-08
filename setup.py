8df76584b647234c2845a2a60a91498303245c0d
vforgione <the.vincedent@icloud.com>
2015-01-08 14:21:06 -0600

    updated setup

--- setup.py
+++ setup.py
@@ -4,8 +4,16 @@ from distutils.core import setup
 setup(
     name='influx-content-client',
     version='0.0.1',
+
     packages=['influx_content_client'],
+
     install_requires=[
         'influxdb==0.1.13'
-    ]
+    ],
+
+    author='Vince Forgione',
+    author_email='vforgione@theonion.com',
+    description='a thin wrapper around the influxdb client to work with content series '
+                '-- see https://github.com/theonion/influx-trending',
+
 )
